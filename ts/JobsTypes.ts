2a9f22020538238c5a85b523c6314440ee496208
dffUqp <yuriivasilchuk.work@gmail.com>
2022-11-28 04:53:16 +0200

    make types for project

---
+++ ts/JobsTypes.ts
@@ -0,0 +1,16 @@
+export type JobsType = {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  title: string;
+  salary: string;
+  address: string;
+  benefits: string[];
+  location: { lat: number; long: number };
+  pictures: string[];
+  createdAt: string;
+  updatedAt: string;
+  description: string;
+  employment_type: string[];
+};
