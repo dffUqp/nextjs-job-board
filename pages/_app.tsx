6657fbf1265263f82fde53ef96495ea2138b3175
dffUqp <yuriivasilchuk.work@gmail.com>
2022-11-28 05:02:19 +0200

    install nextjs-progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,25 @@ import type { AppProps } from 'next/app';
 import { persistor, wrapper } from '../store';
 
 import { Provider } from 'react-redux';
+import Head from 'next/head';
+import NextNProgress from 'nextjs-progressbar';
 import { PersistGate } from 'redux-persist/integration/react';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps): JSX.Element {
   const { store, props } = wrapper.useWrappedStore(pageProps);
 
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor} loading={<Component {...props} />}>
-        <Component {...props} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>{'Job Board'}</title>
+      </Head>
+      <NextNProgress />
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={<Component {...props} />}>
+          <Component {...props} />
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
 
